b76c85826f5b114a3c82b4dcad96bb77d52e4af1
NLuffy <nishant.s.3040@gmail.com>
2023-01-19 20:50:19 +0530

    Initial commit

---
+++ src/main/java/com/example/simplefileshare/simplefileshare/SimplefileshareApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplefileshare.simplefileshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplefileshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplefileshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simplefileshare/simplefileshare/SimplefileshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplefileshare.simplefileshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplefileshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
