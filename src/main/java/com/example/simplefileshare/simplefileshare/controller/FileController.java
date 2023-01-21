b64b94bfeb6e030e4fe37017139c916d1a99056e
NLuffy <nishant.s.3040@gmail.com>
2023-01-21 18:33:29 +0530

    Incorporated PR comment changes

--- src/main/java/com/example/simplefileshare/simplefileshare/controller/FileController.java
+++ src/main/java/com/example/simplefileshare/simplefileshare/controller/FileController.java
@@ -3,15 +3,8 @@ import org.springframework.web.bind.annotation.*;
 import java.io.File;
 
 @RestController
-@RequestMapping("/api/upload")
+@RequestMapping("/api/files")
 public class FileController {
-
-    @GetMapping("/api/get")
-    public String get(File uploadFile){
-        //not needed
-        return "File Get";
-    }
-    
     @PostMapping
     public String upload(File uploadFile){
         //Upload File to S3 Logic
--- src/main/java/com/example/simplefileshare/simplefileshare/data/Filedata.java
+++ src/main/java/com/example/simplefileshare/simplefileshare/data/Filedata.java
@@ -1,17 +1,15 @@
 package com.example.simplefileshare.simplefileshare.data;
 
+import lombok.Getter;
+import lombok.Setter;
 
+
+@Getter
+@Setter
 public class Filedata {
     //@Id
     private String Id;
     private String path;
+    private String name;
 
-    public String getId(){
-        return Id;
-    }
-
-    public String getPath(){
-        return path;
-    }
-    
 }
