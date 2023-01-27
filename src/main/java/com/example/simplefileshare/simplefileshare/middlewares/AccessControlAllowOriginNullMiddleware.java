33ffd1bd8bb6a84a33cfc1e30e9d2eeb6c49a55d
Shashank Vats <shashank.vats8@gmail.com>
2023-01-27 15:41:25 +0530

    Fixed issues in frontend integration

---
+++ src/main/java/com/example/simplefileshare/simplefileshare/middlewares/AccessControlAllowOriginNullMiddleware.java
@@ -0,0 +1,21 @@
+package com.example.simplefileshare.simplefileshare.middlewares;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Profile("dev")
+@Component
+public class AccessControlAllowOriginNullMiddleware extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "null");
+        filterChain.doFilter(request, response);
+    }
+}
