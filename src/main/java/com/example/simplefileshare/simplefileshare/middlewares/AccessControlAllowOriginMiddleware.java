c2de165954bdae14176ad13c29cae653ae7a3ca3
Shashank Vats <shashank.vats8@gmail.com>
2023-02-03 17:25:28 +0530

    Fixed CORS issues

--- src/main/java/com/example/simplefileshare/simplefileshare/middlewares/AccessControlAllowOriginMiddleware.java
+++ src/main/java/com/example/simplefileshare/simplefileshare/middlewares/AccessControlAllowOriginMiddleware.java
@@ -4,18 +4,16 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Profile("dev")
 @Component
-public class AccessControlAllowOriginNullMiddleware extends OncePerRequestFilter {
+public class AccessControlAllowOriginMiddleware extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.addHeader("Access-Control-Allow-Origin", "null");
+        response.addHeader("Access-Control-Allow-Origin", "*");
         filterChain.doFilter(request, response);
     }
 }
